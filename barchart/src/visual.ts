732e6fa689e053ba87822434383ba1718c32bb2d
Hideki Tanaka <hideki.tanaka@aihubble.com>
2020-08-02 07:34:27 +0900

    init

---
+++ barchart/src/visual.ts
@@ -0,0 +1,54 @@
+/*
+ *  Power BI Visual CLI
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+"use strict";
+
+import "core-js/stable";
+import { select } from 'd3';
+import powerbi from "powerbi-visuals-api";
+import "./../style/visual.less";
+import { VisualSettings } from "./settings";
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+
+export class Visual implements IVisual {
+  private target: HTMLElement;
+  private updateCount: number;
+  private settings: VisualSettings;
+  private textNode: Text;
+
+  constructor(options: VisualConstructorOptions) {
+      select(options.element)
+      .append('svg')
+      .append('rect')
+      .attr('width', 50)
+      .attr('height', 50)
+      .attr('fill', 'red');
+      
+  }
+
+  public update(options: VisualUpdateOptions) {}
+}
